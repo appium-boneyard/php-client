8c9f038aca74425c638bff2a6f1c65b7c2b67fcf
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2018-09-13 21:37:08 +0900

    third argument is no needed

--- PHPUnit/Extensions/AppiumTestCase/SessionStrategy/Isolated.php
+++ PHPUnit/Extensions/AppiumTestCase/SessionStrategy/Isolated.php
@@ -21,7 +21,7 @@ class PHPUnit_Extensions_AppiumTestCase_SessionStrategy_Isolated
 {
     public function session(array $parameters)
     {
-        $seleniumServerUrl = PHPUnit_Extensions_Selenium2TestCase_URL::fromHostAndPort($parameters['host'], $parameters['port'], $parameters['secure']);
+        $seleniumServerUrl = PHPUnit_Extensions_Selenium2TestCase_URL::fromHostAndPort($parameters['host'], $parameters['port']);
         $driver = new PHPUnit_Extensions_AppiumTestCase_Driver($seleniumServerUrl, $parameters['seleniumServerRequestsTimeout']);
         $capabilities = array_merge($parameters['desiredCapabilities'],
                                     array(
